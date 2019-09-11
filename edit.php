5bbbe2aec65c30b11d97b2cd876b482f68582b98
RowyDonker <Rowydonker@live.nl>
2019-09-11 21:54:02 +0200

    Aanpassing Edit.php

--- edit.php
+++ edit.php
@@ -1,22 +1,19 @@
 <?php
     session_start();
-	include('db.php');
-	$id=$_GET['id'];
-	$query=mysqli_query($conn,"SELECT * FROM `users` WHERE id='$id'");
-	$row=mysqli_fetch_array($query);
+    echo 'PHP version is: ' . phpversion();
+    echo 'PHP version is: ' . PHP_VERSION;
+    include ('db.php');
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM users WHERE id = $id";
+    $result = $con->query($sql);
+    
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["fst_name"]. " " . $row["lst_name"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    $con->close();
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-<title>Basic MySQLi Commands</title>
-</head>
-<body>
-	<h2>Edit</h2>
-	<form method="POST" action="update.php?id=<?php echo $id; ?>">
-		<label>Firstname:</label><input type="text" value="<?php echo $row['fst_name']; ?>" name="fst_name">
-		<label>Lastname:</label><input type="text" value="<?php echo $row['lst_name']; ?>" name="lst_name">
-		<input type="submit" name="submit">
-		<a href="index.php">Back</a>
-	</form>
-</body>
-</html>
